07dc6079439b1d485697ffbafb7ab7d257ae75e9
Emily <sleman.m@campus.technion.ac.il>
2022-05-11 23:35:02 +0300

    Player.cpp, almost ready, assignment operator isn't there Player.h, not ready at all

--- Player.cpp
+++ Player.cpp
@@ -1,13 +1,105 @@
 #include "Player.h"
+#include <iostream>
 #define INITIAL_LEVEL 1
 #define INITIAL_COINS 0
 
-Player::Player(char *name, int max_hp ,int force)
+Player::Player(const char* name, int maxHp, int force)
 {
     m_name = name;
     m_level = INITIAL_LEVEL;
     m_coins = INITIAL_COINS;
     m_force = force;
-    m_maxHP = max_hp;
-    m_HP = max_hp;
+    m_maxHp = maxHp;
+    m_hp = m_maxHp;
+}
+
+Player::~Player()
+{
+    delete m_name;
+}
+
+Player::Player(const Player& player):
+    m_name(player.m_name),
+    m_hp(player.m_hp),
+    m_level(player.m_level),
+    m_force(player.m_force),
+    m_maxHp(player.m_maxHp),
+    m_coins(player.m_coins) {}
+
+void Player::printPlayerInfo()
+{
+    std::cout << "Player Details:" << std::endl;
+    std::cout << "Name: " << m_name << std::endl;
+    std::cout << "Level: " << m_level << std::endl;
+    std::cout << "Force: " << m_force << std::endl;
+    std::cout << "HP: " << m_hp << std::endl;
+    std::cout << "Coins: " << m_coins << std::endl;
+    std::cout << "------------------------" << std::endl;
+}
+
+void Player::levelUp()
+{
+    if (m_level < 10)
+    {
+        m_level++;
+    }
+}
+
+int Player::getLevel()
+{
+    return m_level;
+}
+
+void Player::buff(int forcePointsToUpgrade)
+{
+    m_force += forcePointsToUpgrade;
+}
+
+void Player::heal(int hpPointsToUpgrade)
+{
+    if (m_hp + hpPointsToUpgrade >= m_maxHp)
+    {
+        m_hp = m_maxHp;
+        return;
+    }
+    m_hp += hpPointsToUpgrade;
+}
+
+void Player::damage(int hpPointsToDowngrade)
+{
+    if (m_hp - hpPointsToDowngrade <= 0)
+    {
+        m_hp = 0;
+        return;
+    }
+    m_hp -= hpPointsToDowngrade;
+}
+
+bool Player::inKnockOut()
+{
+    if (m_hp == 0)
+    {
+        return true;
+    }
+    return false;
+}
+
+void Player::addCoins(int coinsToAdd)
+{
+    m_coins += coinsToAdd;
+}
+
+bool Player::pay(int coinsToPay)
+{
+    if (coinsToPay > m_coins)
+    {
+        return false;
+    }
+    m_coins -= coinsToPay;
+    return true;
+}
+
+int Player::getAttackStrength()
+{
+    return m_force + m_level;
 }
--- Player.h
+++ Player.h
@@ -1,21 +1,34 @@
 #ifndef HW2_PLAYER_H
 #define HW2_PLAYER_H
-
+#define DEFAULT_FORCE 5
+#define DEFAULT_MAX_HP 100
 
 class Player {
 
-    char* m_name; /** English letters. no spaces */
-    int m_HP; /** [0,maxHP] */
+    const char* m_name; /** English letters. no spaces */
+    int m_hp; /** [0,maxHP] */
     int m_level; /** [1,10] */
     int m_force; /** natural number */
-    int m_maxHP; /** natural number */
+    int m_maxHp; /** natural number */
     int m_coins; /** non-negative integer */
 
 
     public:
-    Player(char* name, int max_hp = 100 ,int force=5);
 
-
+    Player(const Player&);
+    Player(const char* name, int maxHp = DEFAULT_MAX_HP, int force = DEFAULT_FORCE);
+    ~Player();
+    Player& operator=(const Player&);
+    void printPlayerInfo();
+    void levelUp();
+    int getLevel();
+    void buff(int forcePointsToUpgrade);
+    void heal(int hpPointsToUpgrade);
+    void damage(int hpPointsToDowngrade);
+    bool inKnockOut();
+    void addCoins(int coinsToAdd);
+    bool pay(int coinsToPay);
+    int getAttackStrength();
 };
 
 
