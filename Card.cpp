365e5b3c7399a364cd284c7a06d4bb3052ff3c1b
Baraa <baraakhalifa@campus.technion.ac.il>
2022-05-13 21:01:46 +0300

    Card take 1

--- Card.cpp
+++ Card.cpp
@@ -11,21 +11,59 @@ void Card::applyEncounter(Player &player) const
     if (m_effect == CardType::Battle)
     {
         //TODO:
+
+        if(m_stats.force<player.getAttackStrength())
+        {
+            player.levelUp();
+            player.addCoins(m_stats.loot);
+        }
+
+        else
+        {
+            player.damage(m_stats.hpLossOnDefeat);
+        }
+
+        printBattleResult(player.isKnockOut());
         return;
     }
     if (m_effect == CardType::Buff)
     {
         //TODO:
+        if(player.pay(m_stats.cost))
+        {
+            player.buff(m_stats.buff);
+        }
         return;
     }
     if (m_effect == CardType::Heal)
     {
         //TODO:
+        if(player.pay(m_stats.cost))
+        {
+            player.heal(m_stats.heal);
+        }
         return;
     }
     if (m_effect == CardType::Treasure)
     {
         //TODO:
+        player.addCoins(m_stats.loot);
         return;
     }
+
+}
+
+void Card::printInfo() const
+{
+    switch(m_effect) {
+        case CardType::Battle :
+            printBattleCardInfo(m_stats);
+        case CardType::Heal   :
+            printHealCardInfo(m_stats);
+        case CardType::Buff   :
+            printBuffCardInfo(m_stats);
+        case  CardType::Treasure:
+            printTreasureCardInfo(m_stats);
+    }
+    return;
 }
